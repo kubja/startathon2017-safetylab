946f80ab71a8edd3d0f5ba2520603ea782d44886
Jakub Tomasek <jakub@tomasek.fr>
2017-10-22 03:43:05 +0800

    Initial commit with RFID library and arduino blinking code

---
+++ arduino.c
@@ -0,0 +1,46 @@
+/*
+Team Safety Lab for PPE enforcement
+
+*/
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
+
+
+
+
+void setup() {                
+  // initialize the digital pin as an output.
+  // Pin 13 has an LED connected on most Arduino boards:
+  pinMode(13, OUTPUT);   
+ 
+  Serial.begin(9600);	// Initialize serial communications with the PC
+  SPI.begin();			// Init SPI bus
+  mfrc522.PCD_Init();	// Init MFRC522 card
+  Serial.println("Scan PICC to see UID and type..."); 
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(1000);              // wait for a second
+  Serial.write("Blinked");
+  
+	// Look for new cards
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+
+	// Dump debug info about the card. PICC_HaltA() is automatically called.
+	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+}
